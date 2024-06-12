c5165e0d8886f0c27638ba551aa6c15cf1a48c55
Baidurya <baiduryadebnath@gmail.com>
2024-06-13 00:32:26 +0530

    First Commit

---
+++ view/precache-manifest.2cda9ab72c6d3a2e158990f51e06dbd5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2f9955b6611e598aca80e459fb9717",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47a68f4e0aa71ac77b48",
+    "url": "/static/css/2.4a711fb7.chunk.css"
+  },
+  {
+    "revision": "d478b47ab092e748efb8",
+    "url": "/static/css/3.941349a6.chunk.css"
+  },
+  {
+    "revision": "a20a232d5ab68270be11",
+    "url": "/static/css/4.9c272d56.chunk.css"
+  },
+  {
+    "revision": "f4d5a0948a573569a38a",
+    "url": "/static/css/5.2d2de32e.chunk.css"
+  },
+  {
+    "revision": "653a870c91647f95272c",
+    "url": "/static/css/6.73ff4e41.chunk.css"
+  },
+  {
+    "revision": "5f8e0286b225d7f10238",
+    "url": "/static/css/main.90595987.chunk.css"
+  },
+  {
+    "revision": "47a68f4e0aa71ac77b48",
+    "url": "/static/js/2.f195809f.chunk.js"
+  },
+  {
+    "revision": "a058c3a53052f2f7676759b51a1267df",
+    "url": "/static/js/2.f195809f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d478b47ab092e748efb8",
+    "url": "/static/js/3.d5bc97b9.chunk.js"
+  },
+  {
+    "revision": "a20a232d5ab68270be11",
+    "url": "/static/js/4.636d2b6f.chunk.js"
+  },
+  {
+    "revision": "f4d5a0948a573569a38a",
+    "url": "/static/js/5.e40aeaf7.chunk.js"
+  },
+  {
+    "revision": "653a870c91647f95272c",
+    "url": "/static/js/6.93d1e934.chunk.js"
+  },
+  {
+    "revision": "f8c4799566b19330dfbf",
+    "url": "/static/js/7.b70fb4d2.chunk.js"
+  },
+  {
+    "revision": "5f8e0286b225d7f10238",
+    "url": "/static/js/main.4474a73b.chunk.js"
+  },
+  {
+    "revision": "49153e5c1211f2f96895",
+    "url": "/static/js/runtime-main.5b0adf6d.js"
+  }
+]);
