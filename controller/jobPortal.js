1d28a269f31b91650ca77797d6ec1aa0f2cc0b1b
Baidurya <baiduryadebnath@gmail.com>
2024-06-13 19:47:43 +0530

    countryCode.json position changed

--- controller/jobPortal.js
+++ controller/jobPortal.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const data = JSON.parse(fs.readFileSync(path.resolve(__dirname , "./countryCode.json") , "utf-8"));
+const data = JSON.parse(fs.readFileSync(path.resolve(__dirname , "../countryCode.json") , "utf-8"));
 
 const getAvailableJobs = async (req , res) => {
     try
