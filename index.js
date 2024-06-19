cb22b0fc9e56df11f5d56d942c979d903dba5936
Baidurya <baiduryadebnath@gmail.com>
2024-06-19 22:27:43 +0530

    defer added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import path from "path";
 import { fileURLToPath } from "url";
+import compression from "compression";
 
 import videoRouter from "./routes/video.js";
 import jobPortalRouter from "./routes/jobPortal.js";
@@ -24,6 +25,7 @@ const databaseURL = process.env.DATABASE_URL;
 app.use(cors());
 app.use(bodyParser.json({limit:"50mb"}));
 app.use(express.json());
+app.use(compression());
 app.use(express.static(path.resolve(__dirname , process.env.FRONTEND)))
 app.use("/api/" , authenticationRouter);
 app.use("/api/" , videoRouter);
